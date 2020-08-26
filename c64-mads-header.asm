5653561f6e02e91e98d542a8d71a0a46a388bdc4
zbyti <zbyti@yahoo.com>
2020-08-27 01:37:04 +0200

    refactor

--- c64-mads-header.asm
+++ c64-mads-header.asm
@@ -4,14 +4,12 @@
 
       org [a($801),a($80b),a(10),b($9e),c'2064',a(0),f(0)],$810
 
-      ldx #$ff
 @     inx
       lda text,x
+      beq end
       sta $400,x
       bne @-
-      lda #$20
-      sta $400,x
-      rts
+end   rts
 
 text: .by +$c0 'MADS'
       .by 0
