78417f0a0686dfa7e9cb679c0c64b1c8d5e2c8f4
zbyti <zbyti@yahoo.com>
2020-08-26 16:30:12 +0200

    refactor

--- c64-mads-header-full.asm
+++ c64-mads-header-full.asm
@@ -4,15 +4,15 @@
 
       org $0801
 
-      ; 10 SYS 2064
-      .byte $01, $08                ; header data, little-endian number indicating the starting address
-      .byte $0C, $08                ; 2-byte pointer to the next line of BASIC code ($080C)
-      .byte $0A, $00                ; 2-byte line number ($000A = 10)
-      .byte $9E                     ; Byte code for the SYS command
-      .byte $20, $32, $30, $36, $34 ; The rest of the line, which is just the string " 2064"
-      .byte $00                     ; Null byte, terminating the line
-      .byte $00, $00                ; 2-byte pointer to the next line of BASIC code ($0000 = end of program)
-      .byte $00, $00                ; filler
+      ; 10 SYS2064
+      .byte $01, $08           ; header data, little-endian number indicating the starting address
+      .byte $0B, $08           ; 2-byte pointer to the next line of BASIC code ($080B)
+      .byte $0A, $00           ; 2-byte line number ($000A = 10)
+      .byte $9E                ; Byte code for the SYS command
+      .byte $32, $30, $36, $34 ; The rest of the line, which is just the string "2064"
+      .byte $00                ; Null byte, terminating the line
+      .byte $00, $00           ; 2-byte pointer to the next line of BASIC code ($0000 = end of program)
+      .byte $00, $00, $00      ; filler
 
       org $0810
 
